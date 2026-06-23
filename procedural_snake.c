7a0ad280cfdd200b99e1b93941eec0a7a2864e69
AlbusAlbatron <albymcn@gmail.com>
2026-06-23 15:40:26 +0100

    Adds line through each circle that rotates in accordance with the circles center 1 up the chain

--- procedural_snake.c
+++ procedural_snake.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <raylib.h>
 #include <raymath.h>
+#include <math.h>
 #include <stdlib.h>
 
 
@@ -8,6 +9,7 @@ typedef struct {
     int line_pos;
     Vector2 center;
     int radius;
+    Vector2 perp_bisector[2];
 } Circle;
 
 
@@ -22,11 +24,17 @@ Circle* circle_generator(int circle_count, Vector2 first_pos, int first_radius){
    circle_line[0].line_pos = 0;
    circle_line[0].center = first_pos;
    circle_line[0].radius = first_radius;
+   circle_line[0].perp_bisector[0] = (Vector2){0.0f, 0.0f};
+   circle_line[0].perp_bisector[1] = (Vector2){0.0f, 0.0f};
+
 
     for (int i = 1; i < circle_count; i++){
 	circle_line[i].line_pos = i;
 	circle_line[i].center = Vector2Subtract(circle_line[i - 1].center, (Vector2){circle_line[i - 1].radius, 0.0f});
-	circle_line[i].radius = (circle_line[i - 1].radius) * 0.8; 
+	circle_line[i].radius = (circle_line[i - 1].radius) * 0.8;
+	circle_line[i].perp_bisector[0] = (Vector2){0, 0};
+	circle_line[i].perp_bisector[1] = (Vector2){0, 0};
+
     }
 
     return circle_line;
@@ -38,6 +46,9 @@ int motion(Circle* circle_line, int circle_count, Vector2 head_pos){
     Vector2 prev_pos = head_pos;
     Vector2 temp_pos;
     
+    int total_steps = 360;
+    float stepSizeDegrees = 360.0f / total_steps;
+
     for (int i = 1; i < circle_count; i++){
 
 	//Check if distance between to centres are greater than radius
@@ -45,10 +56,27 @@ int motion(Circle* circle_line, int circle_count, Vector2 head_pos){
 
 	    //Move circle i towards circle [i-1]'s last postion
 	    temp_pos = circle_line[i].center;
+	    Vector2 direction = {prev_pos.x - temp_pos.x, prev_pos.y - temp_pos.y};
+	    Vector2 norm_direction = Vector2Normalize(direction);
+
 	    circle_line[i].center = Vector2Lerp(circle_line[i].center, prev_pos, 0.03f);
+	    //Tangent position is centre +- radius (depending on rotation) 
+	    //To get rotation of circle find where a diameter length line would be perpendicularly bisected
+	    //
+	    Vector2 norm_bisector_direction = { -norm_direction.y, norm_direction.x};		
+	    circle_line[i].perp_bisector[0] = (Vector2){ temp_pos.x + (norm_bisector_direction.x * circle_line[i].radius),
+				     temp_pos.y + (norm_bisector_direction.y * circle_line[i].radius)
+	    };
+
+	    norm_bisector_direction = (Vector2){ norm_direction.y, -norm_direction.x };
+	    circle_line[i].perp_bisector[1] = (Vector2){ temp_pos.x + (norm_bisector_direction.x * circle_line[i].radius),
+				     temp_pos.y + (norm_bisector_direction.y * circle_line[i].radius)
+	    };
+
+
 	    prev_pos = temp_pos;
-	     
-	}
+	    } 
+	
 	else{
 	    prev_pos = circle_line[i].center;
 	}
@@ -56,8 +84,6 @@ int motion(Circle* circle_line, int circle_count, Vector2 head_pos){
     
     return 0;
 }
-
-
 
 
 
@@ -105,6 +131,7 @@ int main(void){
 	    for(int i = 0; i < circle_count;i++){
 		DrawCircleLines(circle_list[i].center.x, circle_list[i].center.y, circle_list[i].radius, BLACK);
 		DrawCircle(circle_list[i].center.x, circle_list[i].center.y, 1, RED);
+		DrawLineV(circle_list[i].perp_bisector[0], circle_list[i].perp_bisector[1], RED);
 	    }
 
 	    EndDrawing();
